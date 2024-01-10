195b5c85974265561a9ac75d4824e7754ab257a3
baxtikdev <ergashevb111@gmail.com>
2024-01-10 23:02:51 +0500

    filter

--- api/filters.py
+++ api/filters.py
@@ -14,29 +14,6 @@ class ProductFilter(filters.BaseFilterBackend):
             param_value = request.query_params.get(field)
             if param_value:
                 filters[f'{field}__exact'] = param_value
-        category = request.query_params.get('category')
-        if category:
-            filters['subcategory__category__id__in'] = category.split(',')
-
-        subcategory = request.query_params.get('subcategory')
-        if subcategory:
-            filters['subcategory__id__in'] = subcategory.split(',')
-
-        brand = request.query_params.get('brand')
-        if brand:
-            filters['brand__id__in'] = brand.split(',')
-
-        id = request.query_params.get('id')
-        if id:
-            filters['id__in'] = id.split(',')
-
-        startPrice = request.query_params.get('startPrice')
-        if startPrice:
-            filters['startPrice__gte'] = startPrice
-
-        toPrice = request.query_params.get('toPrice')
-        if toPrice:
-            filters['toPrice__lte'] = toPrice
 
         if request.query_params.get('isTop'):
             filters['isTop'] = True
@@ -45,6 +22,31 @@ class ProductFilter(filters.BaseFilterBackend):
             filters['status'] = request.query_params.get('status')
 
         queryset = queryset.filter(**filters)
+
+        category = request.query_params.get('category')
+        if category:
+            queryset = queryset.filter(subcategory__category_id__in=category.split(','))
+
+        subcategory = request.query_params.get('subcategory')
+        if subcategory:
+            queryset = queryset.filter(subcategory_id__in=subcategory.split(','))
+
+        brand = request.query_params.get('brand')
+        if brand:
+            queryset = queryset.filter(brand__id__in=brand.split(','))
+
+        id = request.query_params.get('id')
+        if id:
+            queryset = queryset.filter(id__in=id.split(','))
+
+        startPrice = request.query_params.get('startPrice')
+        if startPrice and startPrice.isdigit():
+            queryset = queryset.filter(startPrice__gte=startPrice)
+
+        toPrice = request.query_params.get('toPrice')
+        if toPrice and toPrice.isdigit():
+            queryset = queryset.filter(toPrice__lte=toPrice)
+
         q = request.query_params.get('q')
         if q:
             q_objects = Q()
