2dea6ff4956d7faccfa2c69aa9a228cf70f92f35
baxtikdev <ergashevb111@gmail.com>
2024-02-04 19:10:46 +0500

    update

--- api/permissions.py
+++ api/permissions.py
@@ -1,13 +1,13 @@
 from rest_framework.permissions import BasePermission
 
-from common.users.models import User
+from common.users.models import UserRole
 
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
         if not request.user.is_authenticated:
             return False
-        return bool(request.user and request.user.is_active and request.user.role == User.UserRole.ADMIN)
+        return bool(request.user and request.user.is_active and request.user.role == UserRole.ADMIN)
 
 
 class IsClient(BasePermission):
@@ -15,7 +15,8 @@ class IsClient(BasePermission):
         if not request.user.is_authenticated:
             return False
         return bool(
-            request.user and request.user.is_active and request.user.is_verified is True and request.user.role == User.UserRole.CLIENT)
+            request.user and request.user.is_active and request.user.is_verified is True and \
+            request.user.role == UserRole.CLIENT)
 
 
 class IsOwn(BasePermission):
