7177af3e5745c53300a443cc980a70a6f585ea1e
baxtikdev <ergashevb111@gmail.com>
2024-02-04 23:04:18 +0500

    order

--- api/payment/payment_utils.py
+++ api/payment/payment_utils.py
@@ -1,22 +1,19 @@
-import base64
-
 from decimal import Decimal
 
-from django.conf import settings
 
-PAYME_ID = settings.PAYME_SETTINGS['ID']
-KEY = "order_id"
+# PAYME_ID = settings.PAYME_SETTINGS['ID']
+# KEY = "order_id"
 
-LINK = 'https://checkout.paycom.uz'
+# LINK = 'https://checkout.paycom.uz'
 
 
-def create_initialization_payme(amount: Decimal, order_id: str) -> str:
-    amount = amount * 100
-    params = f"m={PAYME_ID};ac.{KEY}={order_id};a={amount};c=https://kale.uz/profile/purchases-history"
-    encode_params = base64.b64encode(params.encode("utf-8"))
-    encode_params = str(encode_params, 'utf-8')
-    url = f"{LINK}/{encode_params}"
-    return url
+# def create_initialization_payme(amount: Decimal, order_id: str) -> str:
+#     amount = amount * 100
+#     params = f"m={PAYME_ID};ac.{KEY}={order_id};a={amount};c=https://kale.uz/profile/purchases-history"
+#     encode_params = base64.b64encode(params.encode("utf-8"))
+#     encode_params = str(encode_params, 'utf-8')
+#     url = f"{LINK}/{encode_params}"
+#     return url
 
 def create_initialization_click(amount: Decimal, order_id: str):
     url = (f"https://my.click.uz/services/pay?"
